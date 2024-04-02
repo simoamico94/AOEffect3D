c3c9dd59b863a9dc849f6c324b6aaed62b920955
simoamico94 <simoamico94@gmail.com>
2024-04-02 21:37:35 +0200

    Imported modules and package.json in template

---
+++ Assets/WebGLTemplates/AoConnect/aoconnectWrapper.js
@@ -0,0 +1,30 @@
+import { connect, createDataItemSigner } from './node_modules/@permaweb/aoconnect'
+
+export async function evaluate(pid, data) {
+    // connect wallet 
+    console.log(pid);
+    console.log(data);
+
+  await globalThis.arweaveWallet.connect([
+    'SIGN_TRANSACTION'
+  ])
+  const messageId = await connect().message({
+    process: pid,
+    signer: createDataItemSigner(globalThis.arweaveWallet),
+    tags: [{ name: 'Action', value: 'Eval' }],
+    data
+  })
+  const result = await connect().result({
+    message: messageId,
+    process: pid
+  })
+  //console.log(result)
+  if (result.Error) {
+    throw new Error(result.Error)
+  }
+  if (result.Output?.data?.output) {
+    return result.Output?.data?.output
+  }
+  return undefined
+
+}
